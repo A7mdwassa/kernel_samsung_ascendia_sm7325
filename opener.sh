cbd8afad8a83ae9d4aa8214542019f6371479e6c
A7mdwassa <ahmdwassam112@gmail.com>
2026-02-02 08:15:15 +0200

    .

--- opener.sh
+++ opener.sh
@@ -2,10 +2,10 @@
 echo "Enter File Path: "
 read FILE
 # Always open the original file
-mousepad "$FILE" &
+subl "$FILE"
 
 # If the path contains ".rej", also open the version without ".rej"
 if [[ "$FILE" == *".rej"* ]]; then
     FILE_NO_REJ="${FILE/.rej/}"
-    mousepad "$FILE_NO_REJ" &
+    subl "$FILE_NO_REJ"
 fi
